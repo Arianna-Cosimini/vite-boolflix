9fd980ec7ed645dad64789b2644c77858b56ca67
arianna-cosimini <arianna.cosimini@gmail.com>
2024-03-07 15:20:32 +0100

    Inserito bandiere nel componente appfilterfilms

--- src/components/AppFilterFilms.vue
+++ src/components/AppFilterFilms.vue
@@ -27,10 +27,19 @@ export default {
             <div v-for="currentFilm in store.filmList" class="card">
 
                 <div class="card-body">
+
                     <img :src="'https://image.tmdb.org/t/p/w342' + currentFilm.poster_path" alt="">
 
                     <h5 class="card-title">{{ currentFilm.title }}</h5>
                     <h6 class="card-subtitle mb-2 text-body-secondary">{{ currentFilm.original_title }}</h6>
+
+                    <img v-if="currentFilm.original_language == 'en'" src="../../public/img/british-flag-.jpg" alt="English Flag">
+                    <img v-else-if="currentFilm.original_language == 'it'" src="../../public/img/italy-italia-flag-of-italy-italian-flag-flag.webp" alt="Italian Flag">
+                    <img v-else-if="currentFilm.original_language == 'fr'" src="../../public/img/fr.png" alt="French Flag">
+                    <img v-else-if="currentFilm.original_language == 'es'" src="../../public/img/illustration-of-spain-flag-vector.jpg" alt="Spain Flag">
+
+
+
                     <p class="card-text">{{ currentFilm.original_language }}</p>
                     <p class="card-text">{{ currentFilm.vote_count }}</p>
                 </div>
@@ -44,11 +53,13 @@ export default {
 
 </template>
 
-<style lang="scss">
-
-
+<style lang="scss" scoped>
 .card {
     width: calc(100% / 5);
 
+}
+
+img{
+    width: 100px;
 }
 </style>
