4caeaea24d0fb51da6b9746248a64ccbfe802b6d
arianna-cosimini <arianna.cosimini@gmail.com>
2024-03-08 11:02:52 +0100

    inserito proprietà in cards item

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -25,10 +25,19 @@ export default {
 
 
 <template>
-<CardsItem></CardsItem>
+    <CardsItem v-for="currentFilm in store.filmList" 
+    :imageCard="currentFilm.poster_path"
+    :titleFilm="currentFilm.title"
+    :originalTitle="currentFilm.original_title"
+    :language="currentFilm.original_language">
+    </CardsItem>
+
+    <!-- <CardsItem v-for="currentSerie in store.series" 
+    :imageCard="currentSerie.poster_path"
+    :titleSerie="currentSerie.name"
+    :originalName="currentSerie.original_name"
+    :language="currentSerie.original_language">
+    </CardsItem> -->
 </template>
 
-<style lang="scss">
-
-
-</style>
+<style lang="scss"></style>
--- src/components/CardsItem.vue
+++ src/components/CardsItem.vue
@@ -10,6 +10,18 @@ export default {
         }
     },
 
+    props: {
+
+        imageCard: String,
+        titleFilm: String,
+        titleSerie: String,
+        originalTitle: String,
+        originalName: String,
+        language: String,
+
+
+    },
+
     name: ' CardsItem',
 
 
@@ -21,27 +33,27 @@ export default {
     <div class="container">
         <div class="cards d-flex flex-wrap">
 
-            <div v-for="currentFilm in store.filmList" class="card">
+            <div class="card">
 
                 <div class="card-body">
 
-                    <img :src="'https://image.tmdb.org/t/p/w342' + currentFilm.poster_path" alt="">
+                    <img :src="'https://image.tmdb.org/t/p/w342' + imageCard" alt="">
 
-                    <h5 class="card-title">{{ currentFilm.title }}</h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">{{ currentFilm.original_title }}</h6>
+                    <h5 class="card-title">{{ titleFilm }}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">{{ originalTitle }}</h6>
                     <div class="flag">
 
-                        <img v-if="currentFilm.original_language == 'en'" src="../../public/img/british-flag-.jpg"
+                        <img v-if="language == 'en'" src="../../public/img/british-flag-.jpg"
                             alt="English Flag">
-                        <img v-else-if="currentFilm.original_language == 'it'"
+                        <img v-else-if="language == 'it'"
                             src="../../public/img/italy-italia-flag-of-italy-italian-flag-flag.webp" alt="Italian Flag">
-                        <img v-else-if="currentFilm.original_language == 'fr'" src="../../public/img/fr.png"
+                        <img v-else-if="language == 'fr'" src="../../public/img/fr.png"
                             alt="French Flag">
-                        <img v-else-if="currentFilm.original_language == 'es'"
+                        <img v-else-if="language == 'es'"
                             src="../../public/img/illustration-of-spain-flag-vector.jpg" alt="Spain Flag">
-                        <img v-else-if="currentFilm.original_language == 'cn'" src="../../public/img/cn-flag.jpg"
+                        <img v-else-if="language == 'cn'" src="../../public/img/cn-flag.jpg"
                             alt="Italian Flag">
-                        <img v-else-if="currentFilm.original_language == 'sv'" src="../../public/img/swiss02_flag.jpg"
+                        <img v-else-if="language == 'sv'" src="../../public/img/swiss02_flag.jpg"
                             alt="Italian Flag">
 
                         <img v-else src="../../public/img/Fashion Minimal Mascot Logo.svg" alt="">
@@ -58,22 +70,22 @@ export default {
 
 
 
-        <div class="cards d-flex flex-wrap">
+        <!-- <div class="cards d-flex flex-wrap">
 
-            <div v-for="currentSerie in store.series" class="card">
+            <div class="card">
 
                 <div class="card-body">
-                    <img :src="'https://image.tmdb.org/t/p/w300' + currentSerie.poster_path" alt="">
+                    <img :src="'https://image.tmdb.org/t/p/w300' + imageCard" alt="">
 
-                    <h5 class="card-title">{{ currentSerie.name }}</h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">{{ currentSerie.original_name }}</h6>
+                    <h5 class="card-title">{{titleSerie }}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">{{ originalName }}</h6>
                     <p class="card-text">{{ currentSerie.overview }}</p>
                     <p class="card-text">{{ currentSerie.vote_average }}</p>
                 </div>
 
             </div>
 
-        </div>
+        </div> -->
 
     </div>
 
